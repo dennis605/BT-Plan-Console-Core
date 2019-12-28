a13598025b5fbdc3c0b206b58061bc45508b6673
Dennis Schönig <dennis605@web.de>
2019-12-28 13:44:35 +0100

    Projektdateien hinzufügen.

---
+++ BT-Plan-Console-Core/Context.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_core
+{
+    public class Context :DbContext
+
+    {
+        public DbSet<Person> Personen { get; set; }
+        public DbSet <Event> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            //optionsBuilder.UseSqlite(@"Data Source=.\BTPlan.db");  
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<PersonenEvent>()
+                .HasKey(t => new { t.PersonId, t.EventId });
+
+            modelBuilder.Entity<PersonenEvent>()
+                .HasOne(p => p.Person)
+                .WithMany(p => p.PersonenEvents)
+                .HasForeignKey(p => p.PersonId)
+                ;
+            modelBuilder.Entity<PersonenEvent>()
+                .HasOne(e => e.Event)
+                .WithMany(e => e.PersonenEvents)
+                .HasForeignKey(e => e.EventId)
+                ;
+        }
+    }
+}
