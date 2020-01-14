ff12b89aaecaf0bec0be31d2973edc5d2eb3609f
Dennis Schönig <dennis605@web.de>
2020-01-14 21:54:27 +0100

    Vorbereitung auf Erstellung für Event

--- BT-Plan-Console-Core/Event.cs
+++ BT-Plan-Console-Core/Event.cs
@@ -23,5 +23,63 @@ namespace ConsoleApp_core
         //public ICollection<Person> Persons { get; set; }
         public virtual List<PersonenEvent> PersonenEvents { get; set; }
 
+
+
+
+        public static void CreateEvent(string vname, string nname, string rolle)
+        {
+            string _rolle = rolle;
+
+            //Implementierung ob Mitarbeiter schon existiert
+
+            if (CheckDBforDuplicate(vname, nname, _rolle))
+            {
+                Event.SaveEventToDB(vname, nname, _rolle);
+            }
+        }
+
+        public static bool CheckDBforDuplicate(string vname, string nnachname, string rolle)
+        {
+            return true;
+            //using (Context db = new Context())
+            //{
+            //    if (db.Personen.Any(x => x.Nachname == nnachname))
+            //    {
+            //        if (db.Personen.Any(x => x.Vorname == vname))
+            //        {
+            //            if (db.Personen.Any(x => x.Rolle == rolle))
+            //            {
+            //                Console.WriteLine($"Person ({vname} {nnachname} mit Rolle {rolle} existiert schon in DB");
+            //                return false;
+
+            //            }
+            //        }
+
+            //    }
+            //    Console.WriteLine("Person kann hinzugefügt werden");
+            //    return true;
+            //}
+        }
+
+        public static void SaveEventToDB(string vname, string nname, string rolle)
+        {
+            using (Context db = new Context())
+            {
+                Person pers = new Person();
+                pers.Vorname = vname;
+                pers.Nachname = nname;
+                pers.Rolle = rolle;
+                db.Personen.Add(pers);
+                var result = db.SaveChanges();
+                Console.WriteLine("Check:Nachname: " + result);
+            }
+        }
+
+
+
+
+
     }
+    
+
 }
